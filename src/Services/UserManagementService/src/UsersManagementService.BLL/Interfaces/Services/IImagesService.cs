ddee376fe639727dc0aac62d91aad6e891280581
Mikita Kliuchnikau <kliuchnikaumikita@gmail.com>
2025-05-13 20:28:38 +0300

    Fix

--- src/Services/UserManagementService/src/UsersManagementService.BLL/Interfaces/Services/IImagesService.cs
+++ src/Services/UserManagementService/src/UsersManagementService.BLL/Interfaces/Services/IImagesService.cs
@@ -6,9 +6,9 @@ namespace UsersManagementService.BLL.Interfaces.Services;
 
 public interface IImagesService
 {
-    Task<Guid> CreateImageAsync(CreateImageCommand user, CancellationToken cancellationToken);
+    Task<Guid> CreateImageAsync(CreateImageCommand image, CancellationToken cancellationToken);
 
-    Task<Guid> UpdateImageAsync(UpdateImageCommand user, CancellationToken cancellationToken);
+    Task<Guid> UpdateImageAsync(UpdateImageCommand image, CancellationToken cancellationToken);
 
-    Task<Guid> DeleteImageAsync(DeleteImageCommand user, CancellationToken cancellationToken);
+    Task<Guid> DeleteImageAsync(DeleteImageCommand image, CancellationToken cancellationToken);
 }
