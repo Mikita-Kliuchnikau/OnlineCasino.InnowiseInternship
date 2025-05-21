9612a17421556acf407a1180e08958dde49e132f
Mikita Kliuchnikau <kliuchnikaumikita@gmail.com>
2025-05-21 17:10:21 +0300

    Add stashed changes

---
+++ src/Services/UserManagementService/tests/IntegrationTests/WebApplicationFactory.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+
+
+namespace UsersManagementService.IntegrationTests;
+
+public class TestingWebAppFactory<TContext> : WebApplicationFactory<Program>, IAsyncLifetime where TContext : DbContext
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:17")
+        .WithDatabase("usersdb")
+        .WithUsername("postgres")
+        .WithPassword("password")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            var descriptor = services
+                .SingleOrDefault(s => s.ServiceType == typeof(DbContextOptions<TContext>));
+
+            if (descriptor is not null)
+                services.Remove(descriptor);
+
+            services.AddDbContext<TContext>(options =>
+            {
+                options
+                    .UseNpgsql(_dbContainer.GetConnectionString())
+                    .UseSnakeCaseNamingConvention();
+            });
+        });
+    }
+    
+    public Task InitializeAsync()
+    {
+        return _dbContainer.StartAsync();
+    }
+
+    public new Task DisposeAsync()
+    {
+        return _dbContainer.StopAsync();
+    }
+}
---
+++ src/Services/UserManagementService/tests/UsersManagementService.BLL.UnitTests/TestEntities/TestUserEntities.cs
@@ -0,0 +1,22 @@
+﻿using NSubstitute;
+using UsersManagementService.BLL.Models.User.CreateUser;
+using UsersManagementService.BLL.Models.User.GetPagedUsers;
+using UsersManagementService.BLL.Models.User.UpdateUser;
+using UsersManagementService.Common.Enums;
+using UsersManagementService.DAL.Interfaces.Repositories;
+
+namespace UsersManagementService.BLL.UnitTests.TestEntities;
+
+public static class TestUserEntities
+{
+    public static readonly CreateUserModel CreateModel = new(Guid.NewGuid(), Guid.NewGuid(), "user", "User@email.com");
+    public static readonly Guid DeleteModel = Guid.NewGuid();
+    public static readonly UpdateUserModel UpdateModel = new(
+        Id: Guid.NewGuid(), AuthId: Guid.NewGuid(), Username: "user", Email: "user@email.com", Balance: 0,
+        VerificationStatus: VerificationStatus.UnVerified, IsBanned: false, FirstName: "user",
+        SecondName: "user", LastName: "user", PassportNumber: "123456789", IdentificationNumber: "987654321");
+    public static readonly Guid GetQuery = Guid.NewGuid();
+    public static readonly GetPagedUsersQuery GetPagedQuery = new(1, 10);
+
+    public static readonly IUsersRepository _usersRepositoryMock = Substitute.For<IUsersRepository>();
+}
