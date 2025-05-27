a230a339053aea5304656c793b61b7b4f08bfbf2
Mikita Kliuchnikau <kliuchnikaumikita@gmail.com>
2025-05-27 14:40:54 +0300

    Add serilog logger
    Mock logger in unit tests

--- src/Services/UserManagementService/tests/UsersManagementService.BLL.UnitTests/TestEntities/TestImageEntities.cs
+++ src/Services/UserManagementService/tests/UsersManagementService.BLL.UnitTests/TestEntities/TestImageEntities.cs
@@ -1,5 +1,7 @@
-﻿using NSubstitute;
+﻿using Microsoft.Extensions.Logging;
+using NSubstitute;
 using UsersManagementService.BLL.Models.Image;
+using UsersManagementService.BLL.Services;
 using UsersManagementService.Common.Enums;
 using UsersManagementService.DAL.Interfaces.Repositories;
 
@@ -10,5 +12,6 @@ public static class TestImageEntities
     public static readonly ImageModel ImageModel = new(Guid.NewGuid(), Guid.NewGuid(), "url.com", ImageType.Default);
     public static readonly Guid DeleteModel = Guid.NewGuid();
 
+    public static readonly ILogger<ImagesService> _loggerMock = Substitute.For<ILogger<ImagesService>>();
     public static readonly IImagesRepository _imagesRepositoryMock = Substitute.For<IImagesRepository>();
 }
