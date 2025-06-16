a5a2825dd06bbeef739e285b46cf925ab283fa3f
Mikita Kliuchnikau <kliuchnikaumikita@gmail.com>
2025-06-16 18:37:02 +0300

    AuthService setup

---
+++ src/Services/AuthService/src/AuthService.Api/Constants/Common.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthService.Api.Constants;
+
+public static class Common
+{
+    public const string SignupHint = "&screen_hint=signup";
+    public const string RequestScope = "openid profile email";
+    public const string ResponseType = "code";
+}
---
+++ src/Services/AuthService/src/AuthService.Api/Constants/ConfigurationConstans.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthService.Api.Constants;
+
+public static class ConfigurationConstans
+{
+    public const string ConfigurationSectionName = "Auth0";
+    public const string DomainKey = "Domain";
+    public const string ClientIdKey = "ClientId";
+    public const string ClientSecretKey = "ClientSecret";
+}
